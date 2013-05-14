3f5c391562e51eb2d7f0dc5b549916ebd9cd7062
Omer Akram <om26er@ubuntu.com>
2013-05-15 00:15:37 +0500

    skip test gracefully instead of commenting out

--- tests/autopilot/camera_app/tests/test_zoom.py
+++ tests/autopilot/camera_app/tests/test_zoom.py
@@ -15,6 +15,7 @@ from autopilot.matchers import Eventually
 from camera_app.tests import CameraAppTestCase
 
 import time
+import unittest
 
 class TestCameraZoom(CameraAppTestCase):
     """Tests the main camera features"""
@@ -54,58 +55,59 @@ class TestCameraZoom(CameraAppTestCase):
 
         self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
 
-    # """Tests the plus and minus buttons"""
-    # def test_plus_minus(self):
-    #     zoom_control = self.main_window.get_zoom_control()
-    #     plus = self.main_window.get_zoom_plus()
-    #     minus = self.main_window.get_zoom_minus()
+    @unittest.skip("Disabled this failing test due to a big in the app")    
+    def test_plus_minus(self):
+        """Tests the plus and minus buttons"""
+        zoom_control = self.main_window.get_zoom_control()
+        plus = self.main_window.get_zoom_plus()
+        minus = self.main_window.get_zoom_minus()
 
-    #     # Test that minus when at minimum zoom does nothing
-    #     self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
-    #     self.pointing_device.move_to_object(minus)
-    #     self.pointing_device.click()
-    #     self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
+        # Test that minus when at minimum zoom does nothing
+        self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
+        self.pointing_device.move_to_object(minus)
+        self.pointing_device.click()
+        self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
 
-    #     # Test that plus moves to some non-minimum value
-    #     # and that minus goes back to the minimum
-    #     self.assertEqual(zoom_control.value, 1.0)
-    #     self.pointing_device.move_to_object(plus)
-    #     self.pointing_device.click()
-    #     self.assertThat(zoom_control.value, Eventually(GreaterThan(1.0)))
+        # Test that plus moves to some non-minimum value
+        # and that minus goes back to the minimum
+        self.assertEqual(zoom_control.value, 1.0)
+        self.pointing_device.move_to_object(plus)
+        self.pointing_device.click()
+        self.assertThat(zoom_control.value, Eventually(GreaterThan(1.0)))
 
-    #     value_before_minus = zoom_control.value
+        value_before_minus = zoom_control.value
 
-    #     self.pointing_device.move_to_object(minus)
-    #     self.pointing_device.click()
-    #     self.assertThat(zoom_control.value, Eventually(LessThan(value_before_minus)))
+        self.pointing_device.move_to_object(minus)
+        self.pointing_device.click()
+        self.assertThat(zoom_control.value, Eventually(LessThan(value_before_minus)))
 
-    #     # Test that keeping the plus button pressed eventually reaches max zoom
-    #     self.pointing_device.move_to_object(plus)
-    #     self.pointing_device.press()
-    #     self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
-    #     self.pointing_device.release()
+        # Test that keeping the plus button pressed eventually reaches max zoom
+        self.pointing_device.move_to_object(plus)
+        self.pointing_device.press()
+        self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
+        self.pointing_device.release()
 
-    #     # Test that plus when at maximum zoom does nothing
-    #     self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
-    #     self.pointing_device.move_to_object(plus)
-    #     self.pointing_device.click()
-    #     self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
+        # Test that plus when at maximum zoom does nothing
+        self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
+        self.pointing_device.move_to_object(plus)
+        self.pointing_device.click()
+        self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
 
-    #     # Test that minus moves to some non-maximum value
-    #     # and that plus goes back up
-    #     self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
-    #     self.pointing_device.move_to_object(minus)
-    #     self.pointing_device.click()
-    #     self.assertThat(zoom_control.value, Eventually(NotEquals(zoom_control.maximumValue)))
+        # Test that minus moves to some non-maximum value
+        # and that plus goes back up
+        self.assertThat(zoom_control.value, Eventually(Equals(zoom_control.maximumValue)))
+        self.pointing_device.move_to_object(minus)
+        self.pointing_device.click()
+        self.assertThat(zoom_control.value, Eventually(NotEquals(zoom_control.maximumValue)))
 
-    #     value_before_plus = zoom_control.value
+        value_before_plus = zoom_control.value
 
-    #     self.pointing_device.move_to_object(plus)
-    #     self.pointing_device.click()
-    #     self.assertThat(zoom_control.value, Eventually(GreaterThan(value_before_plus)))
+        self.pointing_device.move_to_object(plus)
+        self.pointing_device.click()
+        self.assertThat(zoom_control.value, Eventually(GreaterThan(value_before_plus)))
 
-    #     # Test that keeping the minus button pressed eventually reaches min zoom
-    #     self.pointing_device.move_to_object(minus)
-    #     self.pointing_device.press()
-    #     self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
-    #     self.pointing_device.release()
+        # Test that keeping the minus button pressed eventually reaches min zoom
+        self.pointing_device.move_to_object(minus)
+        self.pointing_device.press()
+        self.assertThat(zoom_control.value, Eventually(Equals(1.0)))
+        self.pointing_device.release()
