056db6160c7dc06459752a7658389534dd6df466
Omer Akram <om26er@ubuntu.com>
2013-11-18 14:55:07 +0500

    simplify the integration test code much

--- tests/autopilot/camera_app/tests/test_gallery_integration.py
+++ tests/autopilot/camera_app/tests/test_gallery_integration.py
@@ -7,22 +7,20 @@
 
 from __future__ import absolute_import
 
-from autopilot import introspection
+from autopilot.introspection import get_proxy_object_for_existing_process
 from autopilot.matchers import Eventually
 from autopilot.platform import model
 from testtools import skipIf
 from testtools.matchers import Equals
 
 from camera_app.tests import CameraAppTestCase
+from unity8 import process_helpers as helpers
 
 import os
-import subprocess
 
 
 @skipIf(model() == 'Desktop', "Phablet only")
 class TestGalleryIntegration(CameraAppTestCase):
-
-    upstart_override = os.path.expanduser("~/.config/upstart/unity8.override")
 
     def setUp(self):
         super(TestGalleryIntegration, self).setUp()
@@ -33,44 +31,17 @@ class TestGalleryIntegration(CameraAppTestCase):
         super(TestGalleryIntegration, self).tearDown()
         os.system("pkill gallery-app")
 
-    def _restart_unity8_in_testability(self):
-        """Adds an upstart override for unity8 and restarts it in
-        testability.
+    def get_unity8_proxy_object(self):
+        pid = helpers._get_unity_pid()
+        return get_proxy_object_for_existing_process(pid)
 
-        """
-        open(self.upstart_override, 'w').write("exec unity8 -testability")
-        # Incase unity8 is not already running, don't fail
-        try:
-            subprocess.check_call(["stop", "-q", "unity8"])
-        except subprocess.CalledProcessError:
-            pass
-
-        subprocess.check_call(["start", "-q", "unity8"])
-
-    def _get_unity8_proxy_object(self):
-        conn = "com.canonical.Shell.BottomBarVisibilityCommunicator"
-        try:
-            return introspection.get_proxy_object_for_existing_process(
-                connection_name=conn)
-        except RuntimeError:
-            # Didn't use logging here because we want to really print
-            # on the screen that unity8 is being restart so that devs
-            # may not think that their test suite hanged incase restarting
-            # unity8 takes a few seconds.
-            print("Could not find autopilot interface for unity8 "
-                  "restarting it in testability mode")
-
-            self._restart_unity8_in_testability()
-            return introspection.get_proxy_object_for_existing_process(
-                connection_name=conn)
-
-    def _get_current_focused_appid(self, unity8):
-        return unity8.select_single("Shell").currentFocusedAppId
+    def get_current_focused_appid(self):
+        return self.select_single("Shell").currentFocusedAppId
 
     def test_gallery_button_opens_gallery(self):
-        unity8 = self._get_unity8_proxy_object()
-        current_focused_app = self._get_current_focused_appid(unity8)
         gallery_button = self.main_window.get_gallery_button()
+        unity8 = self.get_unity8_proxy_object()
+        current_focused_app = unity8.get_current_focused_appid()
 
         self.pointing_device.click_object(gallery_button)
 
