531431d79fb7abfac1852c5c16681cd6c18210c6
Ugo Riboni <ugo.riboni@canonical.com>
2015-03-10 10:46:26 +0100

    Explicit rate parameter in calls to drag

--- tests/autopilot/camera_app/emulators/panel.py
+++ tests/autopilot/camera_app/emulators/panel.py
@@ -39,7 +39,7 @@ class Panel(CameraCustomProxyObjectBase):
         start_y = y + self.height - 1
         stop_y = y
 
-        self.pointing_device.drag(line_x, start_y, line_x, stop_y, sys.maxsize)
+        self.pointing_device.drag(line_x, start_y, line_x, stop_y, rate=sys.maxsize)
 
     @autopilot_logging.log_action(logger.info)
     def close(self):
