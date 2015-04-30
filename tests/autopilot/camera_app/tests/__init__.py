51b0c6ddb04eb804ba2ae49b0f0aa350b45972a5
Omer Akram <om26er@ubuntu.com>
2015-04-30 22:16:49 +0500

    no need to write a method to check if a directory exists, rather make direct call to os.path.exists

--- tests/autopilot/camera_app/tests/__init__.py
+++ tests/autopilot/camera_app/tests/__init__.py
@@ -90,21 +90,18 @@ class CameraAppTestCase(AutopilotTestCase):
         return MainWindow(self.app)
 
     def delete_all_media(self):
-        if directory_exist(self.pictures_dir):
+        if os.path.exists(self.pictures_dir):
             delete_all_files_in_directory(self.pictures_dir)
 
-        if directory_exist(self.videos_dir):
+        if os.path.exists(self.videos_dir):
             delete_all_files_in_directory(self.videos_dir)
 
     def delete_all_files_in_directory(self, directory):
         files = os.listdir(directory)
-            for f in files:
-                f = os.path.join(directory, f)
-                if os.path.isfile(f):
-                    os.remove(f)
-
-    def directory_exist(self, directory):
-    	return os.path.exists(directory)
+        for f in files:
+            f = os.path.join(directory, f)
+            if os.path.isfile(f):
+                os.remove(f)
 
     def add_sample_photo(self):
         shutil.copyfile(os.path.join(self.sample_dir, "sample.jpg"),
