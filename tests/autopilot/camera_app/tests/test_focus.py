300f5a50992b3e21de820bd689d9c568390ce147
Florian Boucault <florian.boucault@canonical.com>
2016-04-08 15:55:22 +0200

    Autopilot test stability fix: more clearance when testing focus on the edges

--- tests/autopilot/camera_app/tests/test_focus.py
+++ tests/autopilot/camera_app/tests/test_focus.py
@@ -50,10 +50,10 @@ class TestFocus(CameraAppTestCase):
         # Then try on the side edges and top edge to verify they
         # are focusable too
         self.verify_focus_ring_after_click_at(focus_ring,
-                                              geometry.globalRect.x + 1, mid_y)
+                                              geometry.globalRect.x + 100, mid_y)
         self.verify_focus_ring_after_click_at(focus_ring,
                                               geometry.globalRect.x +
-                                              geometry.globalRect.width - 1,
+                                              geometry.globalRect.width - 100,
                                               mid_y)
         self.verify_focus_ring_after_click_at(focus_ring, mid_x, 1)
 
@@ -68,10 +68,10 @@ class TestFocus(CameraAppTestCase):
         # Then try on the left, right and above the center to verify they
         # are focusable too
         self.verify_focus_ring_after_click_at(focus_ring,
-                                              geometry.globalRect.x + 1, mid_y)
+                                              geometry.globalRect.x + 100, mid_y)
         self.verify_focus_ring_after_click_at(focus_ring,
                                               geometry.globalRect.x +
-                                              geometry.globalRect.width - 1,
+                                              geometry.globalRect.width - 100,
                                               mid_y)
         self.verify_focus_ring_after_click_at(focus_ring, mid_x, 1)
 
